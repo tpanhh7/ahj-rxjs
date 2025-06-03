70b19805f288fcff3b56fa6d1a5e93c3b7ea03ec
Tuan Anh <himura.270821@gmail.com>
2025-06-03 15:10:40 +0300

    update api url

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+import webpack from "webpack";
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
 import TerserPlugin from "terser-webpack-plugin";
@@ -8,4 +9,12 @@ export default merge(common, {
     minimize: true,
     minimizer: [new TerserPlugin({})],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      "process.env.API_URL": JSON.stringify(
+        "https://ahj-rxjs-dlmdfqpoy-tuan-anhs-projects-4110927b.vercel.app/api/messages/unread"
+      ),
+    }),
+  ],
 });
